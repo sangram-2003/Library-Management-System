fdc6b61edbc41a4571707e2748c19f63aa61e811
Sangram Bauri <cste.sangram2003@gmail.com>
2026-03-15 22:27:02 +0530

    modify .gitignore

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+function Layout() {
+  return (
+    <div className="flex">
+
+      
+      <div className="w-64 h-screen bg-white shadow-lg shadow-gray-500/50 text-white p-4 fixed left-0 top-0">
+        {/* Sidebar */}
+      </div>
+
+      {/* Main Content */}
+      <div className="ml-64 flex-1 p-4 bg-stone-50 min-h-screen overflow-y-auto">
+        
+        <div className="w-full  text-white p-4">
+          <Outlet/>
+        </div>
+
+        
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Dashboard from './pages/dashboard/Dashboard.jsx';
+import AddBook from './pages/dashboard/AddBook.jsx';
+import AddMember from './pages/dashboard/AddMember.jsx';
+import Books from './pages/dashboard/Books.jsx';
+import IssueBook from './pages/dashboard/IssueBook.jsx';
+import Members from './pages/dashboard/Members.jsx';
+import Reports from './pages/dashboard/Reports.jsx'
+import ReturnBook from './pages/dashboard/ReturnBook.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:[
+      {
+        path:"/dashboard",
+        element:<Dashboard/>
+      },
+      {
+        path:"/add-member",
+        element:<AddMember/>
+      },
+      {
+        path:"/add-books",
+        element:<AddBook/>
+      },
+      {
+        path:"/books",
+        element:<Books/>
+      },
+      {
+        path:"/reports",
+        element:<Reports/>
+      }
+    ]
+  }
+]);
+
+<RouterProvider router={router} />
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
